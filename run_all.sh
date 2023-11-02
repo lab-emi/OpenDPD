c9ea3d8408f64122c120a109f6f1ed8f71cb488b
Chang Gao <gaochangw@outlook.com>
2023-11-02 20:19:41 +0100

    Improved run_all.sh script.

--- run_all.sh
+++ run_all.sh
@@ -16,7 +16,7 @@ devices=0
 
 # Hyperparameters
 seed=0
-n_epochs=1
+n_epochs=100
 frame_length=50
 frame_stride=1
 loss_type=l2
@@ -30,31 +30,9 @@ decay_factor=0.5
 patience=10
 
 #########################
-# Train PA
+# Train
 #########################
 seed=(0)
-step=train_pa
-
-# PA Model
-PA_backbone=(dgru fcn gru vdlstm gmp)
-PA_hidden_size=(8 20 11 8 8)
-PA_num_layers=(1 2 1 1 1)
-
-for i_seed in "${seed[@]}"; do
-    for ((i=0; i<${#PA_backbone[@]}; i++)); do
-        python main.py --dataset_name "$dataset_name" --seed "$i_seed" --step "$step"\
-        --accelerator "$accelerator" --devices "$devices"\
-        --PA_backbone "${PA_backbone[$i]}" --PA_hidden_size "${PA_hidden_size[$i]}" --PA_num_layers "${PA_num_layers[$i]}"\
-        --frame_length "$frame_length" --frame_stride "$frame_stride" --loss_type "$loss_type" --opt_type "$opt_type"\
-        --batch_size "$batch_size" --batch_size_eval "$batch_size_eval" --n_epochs "$n_epochs" --lr_schedule "$lr_schedule"\
-        --lr "$lr" --lr_end "$lr_end" --decay_factor "$decay_factor" --patience "$patience" || exit 1;
-    done
-done
-
-#########################
-# Train DPD
-#########################
-step=train_dpd
 
 # PA Model
 PA_backbone=(dgru fcn gru vdlstm gmp)
@@ -68,33 +46,30 @@ DPD_num_layers=(1 2 1 1 1)
 
 for i_seed in "${seed[@]}"; do
     for ((i=0; i<${#PA_backbone[@]}; i++)); do
+        # Train PA
+        step=train_pa
         python main.py --dataset_name "$dataset_name" --seed "$i_seed" --step "$step"\
-        --accelerator "$accelerator" --devices "$devices"\
-        --PA_backbone "${PA_backbone[$i]}" --PA_hidden_size "${PA_hidden_size[$i]}" --PA_num_layers "${PA_num_layers[$i]}"\
-        --DPD_backbone "${DPD_backbone[$i]}" --DPD_hidden_size "${DPD_hidden_size[$i]}" --DPD_num_layers "${DPD_num_layers[$i]}"\
-        --frame_length "$frame_length" --frame_stride "$frame_stride" --loss_type "$loss_type" --opt_type "$opt_type"\
-        --batch_size "$batch_size" --batch_size_eval "$batch_size_eval" --n_epochs "$n_epochs" --lr_schedule "$lr_schedule"\
-        --lr "$lr" --lr_end "$lr_end" --decay_factor "$decay_factor" --patience "$patience" || exit 1;
-    done
-done
-
-#########################
-# Run DPD
-#########################
-step=run_dpd
-
-# DPD Model
-DPD_backbone=(dgru fcn gru vdlstm gmp)
-DPD_hidden_size=(8 20 11 8 8)
-DPD_num_layers=(1 2 1 1 1)
-
-for i_seed in "${seed[@]}"; do
-    for ((i=0; i<${#DPD_backbone[@]}; i++)); do
+          --accelerator "$accelerator" --devices "$devices"\
+          --PA_backbone "${PA_backbone[$i]}" --PA_hidden_size "${PA_hidden_size[$i]}" --PA_num_layers "${PA_num_layers[$i]}"\
+          --frame_length "$frame_length" --frame_stride "$frame_stride" --loss_type "$loss_type" --opt_type "$opt_type"\
+          --batch_size "$batch_size" --batch_size_eval "$batch_size_eval" --n_epochs "$n_epochs" --lr_schedule "$lr_schedule"\
+          --lr "$lr" --lr_end "$lr_end" --decay_factor "$decay_factor" --patience "$patience" || exit 1;
+        # Train DPD
+        step=train_dpd
         python main.py --dataset_name "$dataset_name" --seed "$i_seed" --step "$step"\
-        --accelerator "$accelerator" --devices "$devices"\
-        --DPD_backbone "${DPD_backbone[$i]}" --DPD_hidden_size "${DPD_hidden_size[$i]}" --DPD_num_layers "${DPD_num_layers[$i]}"\
-        --frame_length "$frame_length" --frame_stride "$frame_stride" --loss_type "$loss_type" --opt_type "$opt_type"\
-        --batch_size "$batch_size" --batch_size_eval "$batch_size_eval" --n_epochs "$n_epochs" --lr_schedule "$lr_schedule"\
-        --lr "$lr" --lr_end "$lr_end" --decay_factor "$decay_factor" --patience "$patience" || exit 1;
+          --accelerator "$accelerator" --devices "$devices"\
+          --PA_backbone "${PA_backbone[$i]}" --PA_hidden_size "${PA_hidden_size[$i]}" --PA_num_layers "${PA_num_layers[$i]}"\
+          --DPD_backbone "${DPD_backbone[$i]}" --DPD_hidden_size "${DPD_hidden_size[$i]}" --DPD_num_layers "${DPD_num_layers[$i]}"\
+          --frame_length "$frame_length" --frame_stride "$frame_stride" --loss_type "$loss_type" --opt_type "$opt_type"\
+          --batch_size "$batch_size" --batch_size_eval "$batch_size_eval" --n_epochs "$n_epochs" --lr_schedule "$lr_schedule"\
+          --lr "$lr" --lr_end "$lr_end" --decay_factor "$decay_factor" --patience "$patience" || exit 1;
+        # Run DPD
+        step=run_dpd
+        python main.py --dataset_name "$dataset_name" --seed "$i_seed" --step "$step"\
+          --accelerator "$accelerator" --devices "$devices"\
+          --DPD_backbone "${DPD_backbone[$i]}" --DPD_hidden_size "${DPD_hidden_size[$i]}" --DPD_num_layers "${DPD_num_layers[$i]}"\
+          --frame_length "$frame_length" --frame_stride "$frame_stride" --loss_type "$loss_type" --opt_type "$opt_type"\
+          --batch_size "$batch_size" --batch_size_eval "$batch_size_eval" --n_epochs "$n_epochs" --lr_schedule "$lr_schedule"\
+          --lr "$lr" --lr_end "$lr_end" --decay_factor "$decay_factor" --patience "$patience" || exit 1;
     done
 done
